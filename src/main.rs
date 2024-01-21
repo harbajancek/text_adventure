56db1c43b563af33e45d4fbff8d956445f9416d6
Jan Harbáček <harbacekjan@gmail.com>
2024-01-21 19:50:11 +0100

    new scene system, not working yet

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,36 @@
 use std::io;
 
 fn main() {
-    // 
-    let intro = r#"
-     _____                _          _          _                          _                         
-    |_   _|   ___   ___  | |_       / \      __| | __   __   ___   _ __   | |_   _   _   _ __    ___ 
-      | |    / _ \ / __| | __|     / _ \    / _` | \ \ / /  / _ \ | '_ \  | __| | | | | | '__|  / _ \
-      | |   |  __/ \__ \ | |_     / ___ \  | (_| |  \ V /  |  __/ | | | | | |_  | |_| | | |    |  __/
-      |_|    \___| |___/  \__|   /_/   \_\  \__,_|   \_/    \___| |_| |_|  \__|  \__,_| |_|     \___|
-                                                                                                     
-    
-    1 - Start
-    2 - Options
-    3 - Exit
-    "#;
-    println!("{intro}");
+    let title = r#"
+ _____                _          _          _                          _                         
+|_   _|   ___   ___  | |_       / \      __| | __   __   ___   _ __   | |_   _   _   _ __    ___ 
+  | |    / _ \ / __| | __|     / _ \    / _` | \ \ / /  / _ \ | '_ \  | __| | | | | | '__|  / _ \
+  | |   |  __/ \__ \ | |_     / ___ \  | (_| |  \ V /  |  __/ | | | | | |_  | |_| | | |    |  __/
+  |_|    \___| |___/  \__|   /_/   \_\  \__,_|   \_/    \___| |_| |_|  \__|  \__,_| |_|     \___|
 
-    let input = get_input_number(3);
+"#
+    .trim_matches('\n');
 
-    match input {
-        1 => println!("Start Adventure!"),
-        2 => println!("Options"),
-        3 => println!("Are you sure you want to exit?"),
-        _ => panic!("Program encountered an error: 1"),
+    let empty_scene: Scene = Scene {id: String::from("empty"), choices: Option::None, text: String::new()};
+    let mut choices = vec![
+        Choice {display_name: String::from("Start"), ..Default::default()},
+        Choice {display_name: String::from("Load"), ..Default::default()},
+        Choice {display_name: String::from("Options"), ..Default::default()},
+        Choice {display_name: String::from("End"), ..Default::default()}];
+    let intro_scene = Scene { id: String::from("intro"), choices: Option::Some(choices), text: String::from(title)};
+
+    let mut global_vars = GlobalVars {
+        ..Default::default()
     };
 
-    println!("Small test git change")
+    let mut current_scene = &intro_scene;
+
+    loop {
+        process_scene(&mut current_scene);
+    }
 }
 
-fn get_input_number(max:u32) -> u32 {
+fn get_choice_input(max: usize) -> usize {
     loop {
         let mut input = String::new();
 
@@ -36,18 +38,62 @@ fn get_input_number(max:u32) -> u32 {
             .read_line(&mut input)
             .expect("Failed to read input");
 
-        let input: u32 = match input.trim().parse() {
+        let input = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please input a number");
                 continue;
-            },
+            }
         };
 
         if (1..=max).contains(&input) {
-            return input
+            return input;
         }
 
         println!("Please input a number between 1 and {max}");
     }
+}
+
+fn process_scene(current_scene : &mut Scene) {
+    println!("{}", current_scene.text);
+
+    if let Some(choices) = &current_scene.choices {
+        for (i, choice) in choices.iter().enumerate() {
+            println!("{i} - {}", choice.display_name);
+        }
+
+        let choice = choices[get_choice_input(choices.len())];
+        
+        if let Some(text) = choice.text {
+            println!("{text}");
+        }
+
+        current_scene = choice.next_scene;
+    } else {
+        println!("1 - Continue");
+        get_choice_input(1);
+        current_scene = current_scene.next_scene.expect("Can't change scene - next scene variable is empty.")
+    }
+}
+
+#[derive(Default)]
+struct GlobalVars {
+    player_name: String,
+    points: i32,
+}
+
+#[derive(Default)]
+struct Choice {
+    next_scene: Option<Scene>,
+    display_name: String,
+    text: Option<String>,
+    changes: Option<fn() -> ()>,
+}
+
+#[derive(Default)]
+struct Scene {
+    id: String,
+    choices: Option<Vec<Choice>>,
+    text: String,
+    next_scene: Option<Scene>,
 }
